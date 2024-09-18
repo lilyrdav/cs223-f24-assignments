dc28dd69349dbbee574e900f6a3908b587d292a9
ldavore1 <ldavore1@swarthmore.edu>
2024-09-17 23:35:35 -0400

    slow.c solution

--- A03/slow.c
+++ A03/slow.c
@@ -4,7 +4,27 @@
  * Description
  ---------------------------------------------*/
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main() {
+  char speech[32];
+  char* pauseSpace;
+  int pauses;
+
+  printf("Pause length: ");
+  scanf("%d", &pauses);
+  printf("Text: ");
+  scanf("%s", speech);
+
+  for (int i = 0; i < strlen(speech); i++) {
+    printf("%c", speech[i]);
+    pauseSpace = (char*)malloc(pauses * sizeof(char));
+    for (int j = 0; j < pauses; j++) {
+      printf(".");
+    }
+    free(pauseSpace);
+  }
+  printf("\n");
   return 0;
 }
